d9bfe8d4608ff40f61d5bbe9dec5e9c741e31c0e
Yazhong Luo <kleistluo@gmail.com>
2016-11-30 20:30:48 -0600

    MVCstyleDairyApp

---
+++ MVCstyleDairy/HomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeTableViewController.h
+//  MVCstyleDairy
+//
+//  Created by Yazhong Luo on 11/30/16.
+//  Copyright © 2016 Yazhong Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewController : UITableViewController
+
+@end
