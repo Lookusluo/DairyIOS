a3d07c600f99c50b82d99acfdcaa2b6c736472fc
Yazhong Luo <kleistluo@gmail.com>
2016-11-30 20:22:29 -0600

    Initial Commit

---
+++ DairyManager.h
@@ -0,0 +1,17 @@
+//
+//  DairyManager.h
+//  MVCstyleDairy
+//
+//  Created by Yazhong Luo on 11/30/16.
+//  Copyright © 2016 Yazhong Luo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DairyManager : NSObject
+@property(nonatomic,strong)NSMutableArray*filesOnDisk;
+@property (nonatomic, strong) NSFileManager *fileManager;
++(instancetype)sharedInstance;
+-(instancetype)init;
+-(void)getAllFilesFromDisk;
+@end
---
+++ DairyManager.m
@@ -0,0 +1,43 @@
+//
+//  DairyManager.m
+//  MVCstyleDairy
+//
+//  Created by Yazhong Luo on 11/30/16.
+//  Copyright © 2016 Yazhong Luo. All rights reserved.
+//
+
+#import "DairyManager.h"
+
+@implementation DairyManager
+-(instancetype)init{
+    if(self = [ super init]){
+        self.filesOnDisk = [[NSMutableArray alloc]init];
+        self.fileManager = [NSFileManager defaultManager];
+    }
+    return self;
+}
++(instancetype)sharedInstance{
+    static id sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc]init];
+    });
+    return sharedInstance;
+}
+
+-(void)getAllFilesFromDisk{
+    NSString *diaryEntriesPath = [NSHomeDirectory() stringByAppendingString:@"/Document/DiaryEntries"];
+    
+    BOOL isDirectory;
+    [self.fileManager fileExistsAtPath:diaryEntriesPath isDirectory:&isDirectory];
+    
+    NSError *error;
+        if (!isDirectory){
+        [self.fileManager createDirectoryAtPath:diaryEntriesPath withIntermediateDirectories:NO attributes:nil error:&error];
+        if (!error){
+            NSLog(@"Directory created");
+        }
+    }
+    self.filesOnDisk = [[self.fileManager contentsOfDirectoryAtPath:diaryEntriesPath error:&error]mutableCopy];
+}
+@end
